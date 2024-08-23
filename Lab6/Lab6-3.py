139f73219736b158df1e85900936ef9b70c48c81
chusanapak fongmanee <141717296+spritermutt@users.noreply.github.com>
2024-08-23 19:32:23 +0700

    Create Lab 6-3.py and Lab 6-4.py

---
+++ Lab6/Lab6-3.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+SW = 22
+LED = 18
+count = 0
+state = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+try:
+    while True:
+        if GPIO.wait_for_edge(SW,GPIO.FALLING):
+            count += 1
+            state = not(state)
+            GPIO.output(LED,state)
+            if state:
+                print(f"Button pressed {count}: LED ON")
+            else:
+                print(f"Button pressed {count}: LED OFF")
+except KeyboardInterrupt:
+    GPIO.cleanup()
+print("\nBye...")
